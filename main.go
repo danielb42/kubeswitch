3794df24ae5b9de5078bd9b20ef5b94a6a2fb78f
danielb <git@masterunit.net>
2018-10-09 22:22:43 +0200

    increase timeout

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func getNamespacesInContextsCluster(context string) []k8s.Namespace {
 			CurrentContext: context}).
 		ClientConfig()
 
-	config.Timeout = 250 * time.Millisecond
+	config.Timeout = 500 * time.Millisecond
 
 	clientset, _ := kubernetes.NewForConfig(config)
 	namespaces, _ := clientset.CoreV1().Namespaces().List(v1.ListOptions{})
